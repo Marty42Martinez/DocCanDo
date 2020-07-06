2bc8c28e8919edbd8691e7b47af9ff30ef0a22f7
Marty42Martinez <manuel.marty.martinez@gmail.com>
2020-07-06 14:20:11 -0700

    Network parser up, documentation included

---
+++ NetworkParser.py
@@ -0,0 +1,76 @@
+from os import listdir, chdir
+from os.path import join, splitext
+import xml.etree.ElementTree as ET
+import csv
+
+networkPath = 'assets/NetworkFiles/'
+networkFilePaths = [join(networkPath, f) for f in listdir(networkPath) if splitext(f)[1] == '.xml']
+
+totalHeader = [
+    "nt_Index",
+    "nt_periodSeqNum",
+    "nt_SiteID",
+    "nt_CardNumber",
+    "nt_CardName",
+    "nt_CardChargesCount",
+    "nt_CardChargesAmount",
+    "nt_CardCorrectionsCount",
+    "nt_CardCorrectionsAmount"
+]
+
+for file in networkFilePaths:
+    tree = ET.parse(file)
+    root = tree.getroot()
+
+    periodSeqNum = root[0].attrib['periodSeqNum']
+    site = root[1].text
+    periodIndex = periodSeqNum + "_" + site
+    # periodEndDate = root[0].attrib['periodEndDate']
+
+    docName = periodSeqNum + "_" + site + "_" + "_" + "network_totals.csv"
+    chdir('parsedDocs/Network/')
+    networkData = open(docName, 'w')
+
+    csvWriter = csv.writer(networkData)
+
+    totals = root[3]
+
+    headerPresent = False
+    
+    index = 0
+    for member in totals.findall('cardInfo'):
+        networkInfo = []
+        if (headerPresent == False):
+            csvWriter.writerow(totalHeader)
+            headerPresent = True
+
+        cardNumber = member.find('cardNumber').text
+        cardName = member.find('cardName').text
+
+        charges = member.find('cardCharges')
+        chargeCount = charges[0].text
+        chargeAmount = charges[1].text
+
+        corrections = member.find('cardCorrections')
+        correctionCount = corrections[0].text
+        correctionAmount = corrections[1].text
+        
+        networkInfo = [
+            periodIndex,
+            periodSeqNum,
+            site,
+            cardNumber,
+            cardName,
+            chargeCount,
+            chargeAmount,
+            correctionCount,
+            correctionAmount
+        ]
+        csvWriter.writerow(networkInfo)
+        index += 1
+
+    networkData.close()
+    chdir('../..')
+    print("Cha'ching! " + str(index) + " rows processed")
+    print("On to the next File!")
+print("Ahh, now I'm done!")
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Set Up for Parsing
 1. clone github repository.
-1. Add 2 Directories/Folders within `parsedDocs`. Name them `Department` and `Period`.
+1. Add 2 Directories/Folders within `parsedDocs`. Name them `Department`,  `Period`, and `Network`.
 1. Add XML files to the proper directories within `assets`.
 1. Follow parser instructions (below).
 1. Retrieve csv files from directories within`parsedDocs`.
@@ -16,3 +16,8 @@
 - Load `department` XML files into `/assets/DepartmentFiles/`.
 - Open GitBash, use `cd` command to get into the DocCanDo directory, then run the following command
     - `python PeriodParser.py`
+
+### NetworkParser
+- Load `network` XML files into `/assets/NetworkFiles/`.
+- Open GitBash, use `cd` command to get into the DocCanDo directory, then run the following command
+    - `python NetworkParser.py`
